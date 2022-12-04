f57500a26cbd0ffc11cabeda23326ad44d1a748b
Gabriele Zanoletti <gabriele.zanoletti@freeloop.it>
2022-12-04 16:23:17 +0100

    Add project files.

---
+++ Festozzo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Festozzo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
