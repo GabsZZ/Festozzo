1fbb00f1b134f2c5294abc6d13433511c4210324
Riccardo Del Gaudio <riccardo.delgaudio@freeloop.it>
2022-12-04 16:27:56 +0100

    suca

--- Festozzo/Program.cs
+++ Festozzo/Program.cs
@@ -6,7 +6,7 @@ namespace Festozzo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Suca");
         }
     }
 }
